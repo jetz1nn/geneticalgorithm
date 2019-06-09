56ecece463b13530be49159831b274367b72cb46
Mathaus <Mathaus@10.40.26.163>
2019-06-09 18:13:15 -0300

    Inicio do desenvolvimendo do Algoritmo

--- Algoritmo Genético/src/principal/Populacao.java
+++ Algoritmo Genético/src/principal/Populacao.java
@@ -19,13 +19,14 @@ public class Populacao {
     private Double txSubstituicao;
     private int[][] posicaoY;
     private int[][] matrizTabuleiro = new int[8][8];
-
+    public int [] individuoFitness;
     public Populacao(int tamanhoPopulacao, Double txMutacao, Double txCruzamento, Double txSubstituicao) {
         this.tamanhoPopulacao = tamanhoPopulacao;
         this.txMutacao = txMutacao;
         this.txCruzamento = txCruzamento;
         this.txSubstituicao = txSubstituicao;
         posicaoY = new int[tamanhoPopulacao][8];
+        individuoFitness = new int [tamanhoPopulacao];
     }
 
     public void generatePopulation() {
@@ -39,49 +40,155 @@ public class Populacao {
 
     public void checkFitness() {
         int[] posicoesY = new int[8];
+        
         for (int i = 0; i < tamanhoPopulacao; i++) {
             for (int j = 0; j < 8; j++) {
+                // popula o tabuleiro com os valores randomizados
                 matrizTabuleiro[j][posicaoY[i][j]] = 1;
+                // guardado os valores os quais 
                 posicoesY[j] = posicaoY[i][j];
             }
-            checkTableFitness(posicoesY);
+            //fitness de cada indivíduo calculado
+            individuoFitness[i] = checkTableFitness(posicoesY);
         }
     }
 
     public int checkTableFitness(int[] posicoesY) {
+        int colisoes = 0;
         for (int i = 0; i < 8; i++) {
-            percorrerLados(posicoesY[i], i);
+            if (percorrerLadoEsquerdo(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerLadoDireito(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerCima(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerCima(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerDiagonalSobeDireita(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerDiagonalSobeEsquerda(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerDiagonalDesceDireita(posicoesY[i], i)) {
+                colisoes++;
+            }
+            if (percorrerDiagonalDesceEsquerda(posicoesY[i], i)) {
+                colisoes++;
+            }
         }
-
-//        return fitness;
+        return colisoes;
     }
 
-    public int percorrerLadoEsquerdo(int posicaoColuna, int linha) {
-        if (linha > 7) {
-            return;
+    public boolean percorrerLadoEsquerdo(int posicaoColuna, int linha) {
+        int valor = linha;
+        while (valor > 0) {
+            valor--;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
         }
-        if () {
-            
-        }
+        return false;
     }
 
-    public void percorrerLadoDireito(int posicaoColuna, int linha) {
-        if (linha < 0) {
-//            return;
+    public boolean percorrerLadoDireito(int posicaoColuna, int linha) {
+        int valor = linha;
+        while (valor < 7) {
+            valor++;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
         }
-
+        return false;
     }
 
-    public void percorrerCima(int posicaoColuna, int linha) {
-
+    public boolean percorrerCima(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        while (valor < 7) {
+            valor++;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void percorrerDiagonais() {
+    public boolean percorrerBaixo(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        while (valor > 0) {
+            valor--;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean percorrerDiagonalSobeDireita(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        int valor2 = linha;
+        // cima sobe direita
+        while (valor < 7 && valor2 < 0) {
+            valor++;
+            valor2--;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean percorrerDiagonalSobeEsquerda(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        int valor2 = linha;
+        // cima sobe direita
+        while (valor > 0 && valor2 > 0) {
+            valor--;
+            valor2--;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean percorrerDiagonalDesceDireita(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        int valor2 = linha;
+        // cima sobe direita
+        while (valor < 7 && valor2 < 7) {
+            valor++;
+            valor2++;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean percorrerDiagonalDesceEsquerda(int posicaoColuna, int linha) {
+        int valor = posicaoColuna;
+        int valor2 = linha;
+        while (valor > 0 && valor2 < 7) {
+            valor--;
+            valor2++;
+            if (matrizTabuleiro[linha][posicaoColuna] == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void unsetTable() {
-
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                matrizTabuleiro[i][j] = 0;
+            }
+        }
     }
 
     public void selectParents() {
